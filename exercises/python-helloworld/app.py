d5597bdce4a04c2d8e640e4a3a7e5f24f79e2099
Mohammed AA Abuibaid <m.a.abuibaid@gmail.com>
2021-07-31 14:26:52 -0400

    adding a CI workflow

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,34 @@
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
+
+@app.route("/status")
+def healthcheck():
+    response=app.response_class(
+        response = json.dumps({"result":"OK - I'm fine"}),
+        status = 200,
+        mimetype='application/json'
+    )
+    app.logger.info('Status request sucessfull')
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response = json.dumps({"status:":"sucess","code":0,"data":{"Usercount":140,"UsercountActive":23}}),
+        status  = 200,
+        mimetype='application/json'
+    )
+    app.logger.info('Metrics request sucessfull')
+    return response
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    app.logger.info('Main request sucessfull')
+    return "Hello World! This is Mohammed"
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
